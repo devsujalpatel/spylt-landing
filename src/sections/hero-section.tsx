855721d56f6a75bdc5c9d59bc52850117bc7cf0d
devsujalpatel <sujal7455@gmail.com>
2026-01-12 13:34:18 +0530

    Hero animation done

--- src/sections/hero-section.tsx
+++ src/sections/hero-section.tsx
@@ -1,19 +1,52 @@
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/all";
+import { gsap } from "gsap";
+
 export const HeroSection = () => {
+  useGSAP(() => {
+    const titleSplit = SplitText.create(".hero-title", {
+      type: "chars",
+    });
+    const tl = gsap.timeline({
+      delay: 1,
+    });
+
+    tl.to(".hero-content", {
+      opacity: 1,
+      y: 0,
+      ease: "power1.inOut",
+    })
+      .to(
+        ".hero-text-scroll",
+        {
+          duration: 1,
+          clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+          ease: "circ.out",
+        },
+        "-=0.5"
+      )
+      .from(titleSplit.chars, {
+        yPercent: 200,
+        stagger: 0.02,
+        ease: "power2.out",
+      }, "-=0.5");
+  });
+
   return (
     <section className="bg-main-bg">
       <div className="hero-container">
         <img
-          src="/images/hero-img.png"
+          src="/images/static-img.png"
           alt="hero-img"
           className="absolute bottom-0 left-1/2 -translate-x-1/2 object-auto scale-100 md:scale-150"
         />
-        <div className="hero-content">
+        <div className="hero-content opacity-0">
           <div className="overflow-hidden">
             <h1 className="hero-title">Freaking Delicious</h1>
           </div>
           <div
             style={{
-              clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
+              clipPath: "polygon(50% 0%, 50% 0%, 50% 100%, 50% 100%)",
             }}
             className="hero-text-scroll"
           >
@@ -25,6 +58,9 @@ export const HeroSection = () => {
             Live life to the fullest with SPYLT: Shatter boredom and embrace
             your inner kid with every deliciously smooth chug.
           </h2>
+          <div className="hero-button">
+            <p>CHUG A SPYLT</p>
+          </div>
         </div>
       </div>
     </section>
