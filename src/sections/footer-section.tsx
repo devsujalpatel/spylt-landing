711aafc4d5f8c71a5cad718b13a4ed4c5b2b5cb0
Sujal Patel <sujal7455@gmail.com>
2026-01-21 23:23:47 +0530

    fixed input

--- src/sections/footer-section.tsx
+++ src/sections/footer-section.tsx
@@ -20,21 +20,6 @@ export const FooterSection = () => {
           </h1>
         </div>
 
-        {isMobile ? (
-          <img
-            src="/images/footer-drink.png"
-            className="absolute top-0 object-contain"
-          />
-        ) : (
-          <video
-            src="/videos/splash.mp4"
-            autoPlay
-            playsInline
-            muted
-            className="absolute top-0 object-contain mix-blend-lighten"
-          />
-        )}
-
         <div className="flex-center gap-5 relative z-10 md:mt-20 mt-5">
           <div className="social-btn">
             <img src="./images/yt.svg" alt="" />
@@ -47,20 +32,20 @@ export const FooterSection = () => {
           </div>
         </div>
 
-        <div className="mt-40 md:px-10 px-5 flex gap-10 md:flex-row flex-col justify-between text-milk font-paragraph md:text-lg font-medium">
+        <div className="mt-40 md:px-10 px-5 z-999 flex gap-10 md:flex-row flex-col justify-between text-milk font-paragraph md:text-lg font-medium">
           <div className="flex items-center md:gap-16 gap-5">
             <div>
-              <p>SPYLT Flavors</p>
+              <a>SPYLT Flavors</a>
             </div>
             <div>
-              <p>Chug Club</p>
-              <p>Student Marketing</p>
-              <p>Dairy Dealers</p>
+              <a>Chug Club</a>
+              <a>Student Marketing</a>
+              <a>Dairy Dealers</a>
             </div>
             <div>
-              <p>Company</p>
-              <p>Contacts</p>
-              <p>Tasty Talk</p>
+              <a>Company</a>
+              <a>Contacts</a>
+              <a>Tasty Talk</a>
             </div>
           </div>
 
@@ -70,25 +55,35 @@ export const FooterSection = () => {
               Updates, Events, and More!
             </p>
             <div className="flex justify-between items-center border-b border-[#D9D9D9] py-5 md:mt-10">
-              {/* The input field and arrow icon for newsletter signup. */}{" "}
-              {/* A
-          border at the bottom for a clean, modern look. */}
               <input
                 type="email"
                 placeholder="Enter your email"
-                className="w-full placeholder:font-sans placeholder:text-[#999999]"
+                className="w-full focus:outline-none z-999 placeholder:font-sans placeholder:text-[#999999]"
               />
               <img src="/images/arrow.svg" alt="arrow" />
             </div>
           </div>
         </div>
 
+        {isMobile ? (
+          <img
+            src="/images/footer-drink.png"
+            className="absolute top-0 object-contain"
+          />
+        ) : (
+          <video
+            src="/videos/splash.mp4"
+            autoPlay
+            playsInline
+            muted
+            className="absolute top-0 z-0 object-contain mix-blend-lighten"
+          />
+        )}
         <div className="copyright-box">
-          {/* The final row with copyright and legal links. */}
           <p>Copyright © 2025 Spylt - All Rights Reserved</p>
           <div className="flex items-center gap-7">
-            <p>Privacy Policy</p>
-            <p>Terms of Sеrvice</p>
+            <a>Privacy Policy</a>
+            <a>Terms of Service</a>
           </div>
         </div>
       </div>
